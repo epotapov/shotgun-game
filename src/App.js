55dc1088ac5ff48f361beee1e30a0d3a649325b9
Edward Potapov <epotapov@comcast.net>
2020-12-24 23:46:14 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+
+var signedin = false;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>This is Shotgun-Game</p>
+        {signedin ? <GameRoom/> : <Lobby/>}
       </header>
+    </div>
+  );
+}
+
+function Lobby() {
+  return(
+    <div>
+      <p>This is Shotgun-Game</p>
+      <input type="text" id="playerNameInput" name="playerNameInput"></input>
+      <button id="playerNameenter" onClick={signedin = true}>Enter</button>
+    </div>
+  );
+}
+
+function GameRoom() {
+  return(
+    <div>
+      <p>You have entered the game room.</p>
     </div>
   );
 }
