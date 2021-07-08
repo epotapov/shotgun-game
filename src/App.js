f1952533915e1362fa0fe78a8528f7f4b51baff5
Edward Potapov <epotapov@comcast.net>
2021-07-08 00:24:08 -0600

    more work on the front end

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function App(props) {
+    const isGamepad = props.isGamepad;
+    if (!isGamepad) {
+        return <HomePage/>;
+    } else {
+        return <Gamepad/>;
+    }
+}
+  
+function HomePage() {
+    return (
+        <div className='Container'>
+            <section className="intro">
+            <h1>/shotgun-game</h1>
+            <div className="buttonholder">
+                <button type="button" onClick={(e) => {
+                    alert(e.target);
+                }}>start game</button>
+            </div>
+            <p>
+                This is shotgun-game. This is based on the rock, paper,
+                scissors <a href="https://www.wikihow.com/Play-the-Shotgun-Game" target="_blank" rel="noreferrer">style game</a>. 
+                You will be able to play this with a friend or by yourself. 
+            </p>
+            </section>
+        </div>
+    );
+}
+
+
+function Gamepad() {
+    const gameid = 67900;
+    const num = 1;
+    const ButtonStyle = {backgroundColor: "#464545", borderColor:"#353434"};
+    const NewButtonStyle = {backgroundColor: "#353434", borderColor:"white"};
+    const [number, changeNumber] = useState(num);
+    const [sheildStyle, changeB1] = useState(ButtonStyle);
+    const [hitStyle, changeB2] = useState(ButtonStyle);
+    const [reloadStyle, changeB3] = useState(ButtonStyle);
+    return (
+        <div className='Container'>
+            <p className="idMarker">game id: {gameid}</p>
+            <button className="exitMarker"><p>x</p></button>
+            <section className="game">
+                <h2 style={{textAlign: "center"}}>{number}</h2>
+                <div className="buttonholder">
+                    <button type="button" onClick={() => {
+                        
+                    }}        
+                    >start game</button>
+                </div>
+                <section className="ActionButtons">
+                    <button style={sheildStyle} 
+                        onClick={() => {
+                            if (sheildStyle === ButtonStyle) {
+                            }
+                            changeB1(NewButtonStyle);
+                            changeB2(ButtonStyle);
+                            changeB3(ButtonStyle);
+                            changeNumber(number - 1);
+                        }}
+                        onMouseEnter={() => {
+                            if(sheildStyle.backgroundColor !== "#353434")
+                                changeB1({...sheildStyle, borderColor: "white"});
+                        }}
+                        onMouseLeave={() =>{
+                            if(sheildStyle.backgroundColor !== "#353434")
+                                changeB1({...sheildStyle, borderColor: "#353434"});
+                        }}
+                    >shield</button>
+                    <button style={hitStyle} 
+                        onClick={() => {
+                            if (hitStyle === ButtonStyle) {
+                            }
+                            changeB1(ButtonStyle);
+                            changeB2(NewButtonStyle);
+                            changeB3(ButtonStyle);
+                            changeNumber(number + 1);
+                        }}
+                        onMouseEnter={() => {
+                            if(hitStyle.backgroundColor !== "#353434") 
+                                changeB2({...hitStyle, borderColor: "white"});
+                        }}
+                        onMouseLeave={() =>{
+                            if(hitStyle.backgroundColor !== "#353434") 
+                                changeB2({...hitStyle, borderColor: "#353434"});
+                        }}
+                    >hit</button>
+                    <button style={reloadStyle} 
+                        onClick={() => {
+                            if (reloadStyle === ButtonStyle) {
+                            }
+                            changeB1(ButtonStyle);
+                            changeB2(ButtonStyle);
+                            changeB3(NewButtonStyle);
+                            changeNumber(0);
+                        }}
+                        onMouseEnter={() => {
+                            if(reloadStyle.backgroundColor !== "#353434")
+                                changeB3({...reloadStyle, borderColor: "white"});
+
+                        }}
+                        onMouseLeave={() =>{
+                            if(reloadStyle.backgroundColor !== "#353434")
+                                changeB3({...reloadStyle, borderColor: "#353434"});
+                        }}
+                    >reload</button>
+                </section>
+            </section>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,60 +1,11 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-function App(props) {
-  const isGamepad = props.isGamepad;
-  if (!isGamepad) {
-    return <HomePage/>;
-  } else {
-    return <Gamepad/>;
-  }
-}
-
-function HomePage() {
-  return (
-    <div className='Container'>
-      <section className="intro">
-        <h1>/shotgun-game</h1>
-        <div className="buttonholder">
-          <button type="button" onClick={(e) => {
-            alert(e.target);
-          }}>start game</button>
-        </div>
-        <p>
-          This is shotgun-game. This is based on the rock, paper,
-           scissors <a href="https://www.wikihow.com/Play-the-Shotgun-Game" target="_blank" rel="noreferrer">style game</a>. 
-          You will be able to play this with a friend or by yourself. 
-        </p>
-      </section>
-    </div>
-  );
-}
-
-
-function Gamepad() {
-  const gameid = 67900;
-  return (
-    <div className='Container'>
-      <p className="idMarker">game id: {gameid}</p>
-      <button className="exitMarker"><p>x</p></button>
-      <section className="game">
-        <div className="buttonholder">
-            <button type="button" onClick={(e) => {
-              alert(e.target);
-            }}>start game</button>
-        </div>
-        <section className="ActionButtons">
-              <button>shield</button>
-              <button>hit</button>
-              <button>reload</button>
-            </section>
-      </section>
-    </div>
-  );
-}
-
-ReactDom.render(
-  <App isGamepad={true} />, 
+ReactDOM.render(
+  <React.StrictMode>
+    <App isGamepad={true}/>
+  </React.StrictMode>, 
   document.getElementById('root')
 );
